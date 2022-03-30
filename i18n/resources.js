8d3c3475683144c58e73a148fef636910dd39ac9
enrimon15 <38566410+enrimon15@users.noreply.github.com>
2022-03-30 16:52:49 +0200

    i18n resources

--- i18n/resources.js
+++ i18n/resources.js
@@ -1,12 +1,180 @@
 export const resources = {
     en: {
         translation: {
-            "Welcome to React": "Welcome to React and react-i18next"
+            new: "New",
+            drawer: {
+                home: "Home",
+                favorites: "Favorites",
+                applications: "Applications",
+                profile: "Profile",
+                settings: "Settings"
+            },
+            splash: {
+                overview: "Find your perfect job offer!",
+                signIn: "Sign in with your account",
+                start: "Start"
+            },
+            auth: {
+                welcome: "Welcome!",
+                login: "Login",
+                register: "Register",
+                profile: "Profile",
+                errorLogin: "Invalid credentials, try again!",
+                email: "Email",
+                name: "Name",
+                surname: "Surname",
+                password: "Password",
+                passwordConfirm: "Confirm Password",
+                emailError: "Email not valid",
+                nameError: "Name not valid",
+                surnameError: "Surname not valid",
+                passwordError: "Password not valid",
+                passwordConfirmError: "The password do not match",
+                errorRegister: "Sign up failed, try again!",
+                errorUpdate: "Update failed, try again!",
+                save: "Save",
+                logout: "Logout"
+            },
+            home: {
+                welcome: "Welcome",
+                title: "Find your perfect job",
+                searchNewJob: "Search new job offer...",
+                popular: "Populars",
+                recent: "Recents"
+            },
+            detail: {
+                experience: "Experience",
+                contract: "Contract",
+                salary: "Salary",
+                descriptionJob: "Job Description",
+                applied: "Already Applied",
+                apply: "Apply",
+                description: "DESCRIPTION",
+                map: "MAP",
+                skills: "SKILLS",
+                full_time: 'Full Time',
+                part_time: 'Part Time',
+                internship: 'Intern',
+                construction_base: 'Intern',
+                years: "years"
+            },
+            alert: {
+                warning: "Warning",
+                confirmTextFavorite: "Your are going to remove a saved job. Do you want to continue?",
+                confirmTextApplication: "Your are going to remove an application. Do you want to continue?",
+                ok: "Ok",
+                cancel: "Cancel"
+            },
+            favorite: {
+                title: "Saved Jobs"
+            },
+            application: {
+                title: "Applications"
+            },
+            search: {
+                aroundMe: "Around me"
+            },
+            settings: {
+                title: "Settings"
+            },
+            error: {
+                warning: "Warning",
+                search: "Search input not valid. Try again!",
+                localization: "You need to allow localization permissions to handle this function",
+                ok: "Ok",
+                generic: "Oops.. Something went wrong!",
+                retry: "Try Again",
+                noData: "No data found!"
+            }
         }
     },
     it: {
         translation: {
-            "Welcome to React": "Benvenuto a react-i18next"
+            new: "Nuova",
+            drawer: {
+                home: "Home",
+                favorites: "Preferiti",
+                applications: "Candidature",
+                profile: "Profilo",
+                settings: "Impostazioni"
+            },
+            splash: {
+                overview: "Trova l'offerta di lavoro adatta a te!",
+                signIn: "Accedi con il tuo account",
+                start: "Inizia"
+            },
+            auth: {
+                welcome: "Benvenuto!",
+                login: "Login",
+                register: "Registrati",
+                profile: "profilo",
+                errorLogin: "Credenziali non valide, riprova!",
+                email: "Email",
+                name: "Nome",
+                surname: "Cognome",
+                password: "Password",
+                passwordConfirm: "Conferma Password",
+                emailError: "Email non valida",
+                nameError: "Nome non valido",
+                surnameError: "Cognome non valido",
+                passwordError: "Password non valida",
+                passwordConfirmError: "La password non coincide",
+                errorRegister: "Registrazione non riuscita, riprova!",
+                errorUpdate: "Modifica non riuscita, riprova!",
+                save: "Salva",
+                logout: "Esci"
+            },
+            home: {
+                welcome: "Bentornato",
+                title: "Trova il lavoro perfetto",
+                searchNewJob: "Cerca un nuovo lavoro...",
+                popular: "Popolari",
+                recent: "Recenti"
+            },
+            detail: {
+                experience: "Esperienza",
+                contract: "Contratto",
+                salary: "Salario",
+                descriptionJob: "Descrizione Offerta",
+                applied: "Candidatura Effettuata",
+                apply: "Candidati",
+                description: "DESCRIZIONE",
+                map: "MAPPA",
+                skills: "COMPETENZE",
+                full_time: 'Full Time',
+                part_time: 'Part Time',
+                internship: 'Tirocinio',
+                construction_base: 'Tirocinio',
+                years: "anni"
+            },
+            alert: {
+                warning: "Attenzione",
+                confirmTextFavorite: "Stai per rimuovere un'offerta salvata. Vuoi proseguire?",
+                confirmTextApplication: "Stai per eliminare una candidatura. Vuoi proseguire?",
+                ok: "Ok",
+                cancel: "Annulla"
+            },
+            favorite: {
+                title: "Offerte Salvate"
+            },
+            application: {
+                title: "Candidature"
+            },
+            search: {
+                aroundMe: "Intorno a me"
+            },
+            settings: {
+                title: "Impostazioni"
+            },
+            error: {
+                warning: "Attenzione",
+                search: "Ricerca non valida. Inserire un testo valido!",
+                localization: "Hai bisogno di fornire i permessi sulla localizzazione per utilizzare questa funzionalità",
+                ok: "Ok",
+                generic: "Oops.. Qualcosa è andato storto!",
+                retry: "Riprova",
+                noData: "Dati non trovati!"
+            }
         }
     }
 };
--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,6 +1,7 @@
 import {NavigationContainer} from "@react-navigation/native";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import {Ionicons} from '@expo/vector-icons';
+import {useTranslation} from "react-i18next";
 import {useSelector} from "react-redux";
 import {sUserData} from "../store/selectors/UserSelector";
 import HomeNavigator from "./HomeNavigator";
@@ -17,6 +18,7 @@ import {ObjectUtils} from "../util/ObjectUtils";
 const Drawer = createDrawerNavigator();
 
 const RootNavigator = () => {
+    const {t} = useTranslation();
     const userLogged = useSelector(sUserData);
     const conditionAuthNavigation = (ObjectUtils.isEmpty(userLogged));
 
@@ -43,27 +45,32 @@ const RootNavigator = () => {
                         drawerIcon: ({color}) => (
                             <Ionicons name="home-outline" size={22} color={color}/>
                         ),
+                        drawerLabel: t('drawer.home')
                     }}/>
                     <Drawer.Screen name={Routes.favorites} component={FavoritesNavigator} options={{
                         drawerIcon: ({color}) => (
                             <Ionicons name="ios-heart-outline" size={22} color={color}/>
                         ),
+                        drawerLabel: t('drawer.favorites')
                     }}/>
                     <Drawer.Screen name={Routes.applications} component={ApplicationsNavigator} options={{
                         drawerIcon: ({color}) => (
                             <Ionicons name="ios-briefcase-outline" size={22} color={color}/>
                         ),
+                        drawerLabel: t('drawer.applications')
                     }}/>
                     <Drawer.Screen name={Routes.profile} component={Profile} options={{
                         drawerIcon: ({color}) => (
                             <Ionicons name="person-outline" size={22} color={color}/>
                         ),
+                        drawerLabel: t('drawer.profile')
                     }}
                     />
                     <Drawer.Screen name={Routes.settings} component={Settings} options={{
                         drawerIcon: ({color}) => (
                             <Ionicons name="settings-outline" size={22} color={color}/>
                         ),
+                        drawerLabel: t('drawer.settings')
                     }}
                     />
                 </Drawer.Navigator>)
